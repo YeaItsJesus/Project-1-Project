bb40feaffa7ace9e74bac4714283aee61af6d646
Omar Zumaya <120762586+Omar4589@users.noreply.github.com>
2023-03-09 12:16:00 -0600

    Update 1, API Urls and first function
    Adds a complete list of URLS that we can use to make API calls.
    Adds boilerplate function that calls latestPhonesUrl to receive information about the latest phones available.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,38 @@
 //API URLS
 
 var latestPhonesUrl = "http://phone-specs-api.azharimm.dev/latest";
+//latest:{"endpoint":"/latest","example":"http://phone-specs-api.azharimm.dev/latest"}
 
 var phoneBrandsUrl = "http://phone-specs-api.azharimm.dev/brands";
+//list_brands:{"endpoint":"/brands","example":"http://phone-specs-api.azharimm.dev/brands"}
 
 var phoneSpecsUrl =
-  "http://phone-specs-api.azharimm.dev/search?query=Iphone 12 pro max";
+  "http://phone-specs-api.azharimm.dev/search?query={phoneNameGoesHere";
+//phone_specs:{"endpoint":"/{phone_slug}","example":"http://phone-specs-api.azharimm.dev/apple_iphone_12_pro_max-10237"}
 
-var searchPhonesUrl =
-  "http://phone-specs-api.azharimm.dev/search?query=Iphone-12-pro-max";
+var searchForPhoneUrl =
+  "http://phone-specs-api.azharimm.dev/search?query={userInputGoesHere}";
+//search:{"endpoint":"/search","example":"http://phone-specs-api.azharimm.dev/search?query=Iphone 12 pro max"}
 
-//Calls API
+var searchTopPhonesByInterest = "http://phone-specs-api.azharimm.dev/top-by-interest"
+//top_by_interest:{"endpoint":"/top-by-interest","example":"http://phone-specs-api.azharimm.dev/top-by-interest"}
+
+
+//Calls phoneSpecs API
+fetch(latestPhonesUrl)
+  .then(function (response) {
+    //Parses response into json
+    return response.json();
+  })
+  .then(function (data) {
+  //console log to review data received
+    console.log(data);
+    //Latest Phones
+
+  });
+
+/*
+//Calls phoneSpecs API
 fetch(phoneSpecsUrl)
   .then(function (response) {
     //Parses response into json
@@ -31,3 +53,4 @@ fetch(phoneSpecsUrl)
         console.log(phoneSpecs);
       });
   });
+*/
