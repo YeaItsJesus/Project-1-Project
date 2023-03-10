08dcffe5257f0f89d4f5c07dbb96b8991fcf36ae
Omar4589 <omar4589@hotmail.com>
2023-03-10 15:49:36 -0600

    Adds function that searches for phone when SearchBar form is submitted and returns phone specs

--- assets/script.js
+++ assets/script.js
@@ -6,14 +6,6 @@ var latestPhonesUrl = "http://phone-specs-api.azharimm.dev/latest";
 var phoneBrandsUrl = "http://phone-specs-api.azharimm.dev/brands";
 //list_brands: Endpoint:"/brands", Example":"http://phone-specs-api.azharimm.dev/brands"
 
-var phoneSpecsUrl =
-  "http://phone-specs-api.azharimm.dev/search?query={phoneNameGoesHere";
-//phone_specs: Endpoint:"/{phone_slug}" ,Example":"http://phone-specs-api.azharimm.dev/apple_iphone_12_pro_max-10237"
-
-var searchForPhoneUrl =
-  "http://phone-specs-api.azharimm.dev/search?query={user input goes here}";
-//search: Endpoint:"/search", Example":"http://phone-specs-api.azharimm.dev/search?query=Iphone 12 pro max"
-
 var searchTopPhonesByInterestUrl =
   "http://phone-specs-api.azharimm.dev/top-by-interest";
 //top_by_interest: Endpoint:"/top-by-interest", Example":"http://phone-specs-api.azharimm.dev/top-by-interest"
@@ -34,7 +26,7 @@ function getLatestPhones() {
     });
 }
 
-getLatestPhones();
+//getLatestPhones();
 
 /*
 //Calls phoneBrands API
@@ -79,27 +71,6 @@ function getLatestPhones() {
   };
 */
 
-//Calls searchForPhone API
-function searchForPhone() {
-  fetch(searchForPhoneUrl)
-    .then(function (response) {
-      //Parses response into json
-      return response.json();
-    })
-    .then(function (data) {
-      //console log to review data received
-      //console.log(data);
-      //Phone
-      var phone = data.data.phones[0];
-      var phoneName = phone.phone_name;
-      var phoneImage = phone.image;
-      var phoneBrand = phone.brand;
-      //Search results return (brand, specs link "detail", image, phone_name)
-    });
-}
-
-searchForPhone();
-
 //Calls searchTopPhonesByInterest API
 function getTopPhones() {
   fetch(searchTopPhonesByInterestUrl)
@@ -115,4 +86,54 @@ function getTopPhones() {
     });
 }
 
-getTopPhones();
+//getTopPhones();
+
+var searchBar = $("#search-bar");
+var searchField = $("#search");
+
+//Searches For a Phone API
+searchBar.submit(function (event) {
+  event.preventDefault(); // prevent default behavior of form submission
+  //Get searchfield value
+  var userInput = searchField.val();
+  //Calls searchForPhone API
+  fetch(`http://phone-specs-api.azharimm.dev/search?query=${userInput}`)
+    .then(function (response) {
+      //Parses response into json
+      return response.json();
+    })
+    .then(function (data) {
+      //console log to review data received
+      console.log(data);
+      //Phone
+      var phone = data.data.phones[0];
+      var phoneName = phone.phone_name;
+      var phoneImage = phone.image;
+      var phoneBrand = phone.brand;
+      var phoneSlug = phone.slug;
+      //Search results return (brand, specs link "detail", image, phone_name)
+
+      //Calls API to get specs
+      fetch(`http://phone-specs-api.azharimm.dev/${phoneSlug}`)
+        .then(function (response) {
+          //Parses response into json
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          //phone specs
+          var phoneBrand = data.data.brand;
+          var phoneName = data.data.phone_name;
+          var releaseDate = data.data.release_date;
+          var storageOptions = data.data.storage;
+          var thumbnail = data.data.thumbnail;
+          var screenSize =
+            data.data.specifications[3].specs[1].val[0].split(",")[0];
+          var mainCamera = data.data.specifications[6].specs[0].val[0];
+          var frontCamera = data.data.specifications[7].specs[0].val[0];
+          var colorOptions = data.data.specifications[12].specs[0].val[0];
+          var phonePrice =
+            data.data.specifications[12].specs[4].val[0].split("/")[0];
+        });
+    });
+});
