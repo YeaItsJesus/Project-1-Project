47232bbb24263989d481a76893fe2bfc4ce5ebfb
Omar4589 <omar4589@hotmail.com>
2023-03-10 22:41:23 -0600

    fixes HTML layout and adds search function

--- assets/script.js
+++ assets/script.js
@@ -18,15 +18,43 @@ function getLatestPhones() {
       return response.json();
     })
     .then(function (data) {
-      //console log to review data received
-      //console.log(data.data.phones);
       //18 Latest Phones
-      //Each listed phones contains an image(phonesList[i].image) and phone name(phonesList[i].phone_name)
+      //Each listed phones contains an image(phonesList[i].image) and phone name(phonesList[i].phone_name and specs (phonesList[i].detail) and phone slug (phonesList[i].slug))
       var phonesList = data.data.phones;
+      //console.log(phonesList);
+      for (i = 0; i < 10; i++) {
+        var phoneName = phonesList[i].phone_name;
+        var phoneImage = phonesList[i].image;
+        var phoneSpecs = phonesList[i].detail;
+        var phoneSlug = phonesList[i].slug;
+        //Calls API to get phone specs
+        fetch(`http://phone-specs-api.azharimm.dev/${phoneSlug}`)
+          .then(function (response) {
+            //Parses response into json
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+            //Get handle on phone specs
+            var phoneBrand = data.data.brand;
+            var phoneName = data.data.phone_name;
+            var releaseDate = data.data.release_date;
+            var storageOptions = data.data.storage;
+            var thumbnail = data.data.thumbnail;
+            var screenSize =
+              data.data.specifications[3].specs[1].val[0].split(",")[0];
+            var mainCamera = data.data.specifications[6].specs[0].val[0];
+            var frontCamera = data.data.specifications[7].specs[0].val[0];
+            var colorOptions = data.data.specifications[12].specs[0].val[0];
+            //var phonePrice = data.data.specifications[12].specs[4].val[0].split("/")[0];
+
+            console.log("---------------");
+          });
+      }
     });
 }
 
-//getLatestPhones();
+getLatestPhones();
 
 /*
 //Calls phoneBrands API
@@ -47,7 +75,7 @@ function getLatestPhones() {
 
 /*
 //Calls phoneSpecs API
-function getLatestPhones() {
+function getPhoneSpecs() {
   fetch(phoneSpecsUrl)
     .then(function (response) {
       //Parses response into json
@@ -63,9 +91,21 @@ function getLatestPhones() {
           //parses response into json
           return response.json();
         })
-        .then(function (phoneSpecs) {
-          //log phone specs array
-          console.log(phoneSpecs);
+        .then(function (data) {
+          console.log(data);
+          //phone specs
+          var phoneBrand = data.data.brand;
+          var phoneName = data.data.phone_name;
+          var releaseDate = data.data.release_date;
+          var storageOptions = data.data.storage;
+          var thumbnail = data.data.thumbnail;
+          var screenSize =
+            data.data.specifications[3].specs[1].val[0].split(",")[0];
+          var mainCamera = data.data.specifications[6].specs[0].val[0];
+          var frontCamera = data.data.specifications[7].specs[0].val[0];
+          var colorOptions = data.data.specifications[12].specs[0].val[0];
+          var phonePrice =
+            data.data.specifications[12].specs[4].val[0].split("/")[0];
         });
     });
   };
@@ -91,7 +131,7 @@ function getTopPhones() {
 var searchBar = $("#search-bar");
 var searchField = $("#search");
 
-//Searches For a Phone API
+//Searches For a Phone when user submits a search
 searchBar.submit(function (event) {
   event.preventDefault(); // prevent default behavior of form submission
   //Get searchfield value
